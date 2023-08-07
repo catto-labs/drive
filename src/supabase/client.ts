28a44c429bc8a5c16ba00e63264e09319c7f9edd
Mikkel RINGAUD <mikkel@milescode.dev>
2023-08-08 01:26:29 +0200

    fix(env): use public vite vars

--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -2,6 +2,6 @@ import { createClient } from "@supabase/supabase-js";
 
 // Create a single Supabase client for interacting with your database
 export const supabase = createClient(
-  process.env.SUPABASE_PROJECT_URL as string,
-  process.env.SUPABASE_ANON_KEY as string
+  import.meta.env.VITE_SUPABASE_PROJECT_URL as string,
+  import.meta.env.VITE_SUPABASE_ANON_KEY as string
 );
