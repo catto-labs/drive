81eb3dcd0b5bd9261fa8899d86a3142e2edc50e5
Mikkel RINGAUD <mikkel@milescode.dev>
2023-08-08 01:22:43 +0200

    fix(vite): add config for importing via `@/`

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 import { defineConfig } from "vite";
+import path from "node:path";
+import fs from "node:fs";
 
 import solid from "solid-start/vite";
 // @ts-expect-error
@@ -10,6 +12,16 @@ export default defineConfig({
   plugins: [
     unocss(),
     solid({ ssr: false, adapter: netlify() })
-  ]
+  ],
+
+  server: {
+    strictPort: true
+  },
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    }
+  }
 });
 
