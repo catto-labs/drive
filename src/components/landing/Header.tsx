bc0dfafdedd57c50c235740092d6dbb2ef68a19d
jason <development@pnxl.dev>
2023-08-09 10:37:47 +0700

    feat: show login/register buttons when logged out

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -1,6 +1,8 @@
-import IconAccount from "~icons/mdi/account";
-
+import { Show } from "solid-js";
 import { A } from "solid-start";
+import { isAuthenticated } from "@/stores/auth";
+
+import IconAccount from "~icons/mdi/account";
 
 export default function Header() {
   return (
@@ -18,14 +20,35 @@ export default function Header() {
         >
           Dashboard
         </A>
-        <A
-          href="/"
-          class="text-crust inline-flex gap-x-1 bg-text hover:bg-subtext0 border border-subtext0 pl-3 pr-4 py-2 rounded-md h-fit my-auto shadow-inner shadow-lg transform duration-150"
+
+        <Show
+          when={isAuthenticated()}
+          fallback={
+            <div class="flex gap-x-4 h-fit">
+              <A
+                href="/auth/login"
+                class="text-crust inline-flex gap-x-1 bg-text hover:bg-subtext0 border border-subtext0 pl-3 pr-4 py-2 rounded-md h-fit my-auto shadow-inner shadow-lg transform duration-150"
+              >
+                Login
+              </A>
+              <A
+                href="/auth/register"
+                class="text-crust inline-flex gap-x-1 bg-text hover:bg-subtext0 border border-subtext0 pl-3 pr-4 py-2 rounded-md h-fit my-auto shadow-inner shadow-lg transform duration-150"
+              >
+                Register
+              </A>
+            </div>
+          }
         >
-          {/* this can be substituted for the user's profile picture instead */}
-          <IconAccount class="text-[16px] my-auto" />
-          My Account
-        </A>
+          <A
+            href="/"
+            class="text-crust inline-flex gap-x-1 bg-text hover:bg-subtext0 border border-subtext0 pl-3 pr-4 py-2 rounded-md h-fit my-auto shadow-inner shadow-lg transform duration-150"
+          >
+            {/* this can be substituted for the user's profile picture instead */}
+            <IconAccount class="text-[16px] my-auto" />
+            My Account
+          </A>
+        </Show>
       </div>
     </header>
   );
