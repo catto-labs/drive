b75f4ca066f2ab50a8d0b2fdc8faa56e3d38d2e7
jason <development@pnxl.dev>
2023-08-09 08:03:15 +0700

    Revert "feat: add form resets to unocss"
    This reverts commit e96b11154b7059467e4490548ae689dd82c758ca.

--- uno.config.ts
+++ uno.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig, presetUno } from "unocss";
+import { extendCatppuccin } from "unocss-catppuccin";
 
 import presetRemToPx from "@unocss/preset-rem-to-px";
 import transformerVariantGroup from "@unocss/transformer-variant-group";
-import { presetForms } from "@julr/unocss-preset-forms";
-import { extendCatppuccin } from "unocss-catppuccin";
 
 export default defineConfig({
   presets: [
     presetUno(),
     presetRemToPx(),
     extendCatppuccin({ prefix: "", defaultVariant: "macchiato" }),
-    presetForms(),
   ],
 
   transformers: [transformerVariantGroup()],
