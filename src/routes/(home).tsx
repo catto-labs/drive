0f323cb008ad006f20d66f4ba24d5bc16cf336c7
jason <development@pnxl.dev>
2023-08-09 07:44:55 +0700

    refactor: remove reference comment

--- src/routes/(home).tsx
+++ src/routes/(home).tsx
@@ -32,10 +32,3 @@ export default function Home() {
     </main>
   );
 }
-
-/*
-<main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl">catto drive</h1>
-      <A href="/dashboard">dashboard!</A>
-    </main>
-    */
