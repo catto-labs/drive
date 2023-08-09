e0d1fc65954f7413bdcd2f1c7d24125ef060eb0a
jason <development@pnxl.dev>
2023-08-09 07:44:19 +0700

    styles: make everything aligned to middle

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -4,26 +4,29 @@ import { A } from "solid-start";
 
 export default function Header() {
   return (
-    <header class="bg-gradient-to-b bg-gradient-from-crust h-64 p-8 flex justify-end gap-x-4">
-      <A
-        href="https://github.com/catto-labs/drive"
-        class="text-crust inline-flex gap-x-1 bg-text hover:bg-subtext0 border border-subtext0 px-4 py-2 rounded-md h-fit shadow-inner shadow-lg transform duration-150"
-      >
-        Command-line Client
-      </A>
-      <A
-        href="/dashboard"
-        class="text-crust mr-6 inline-flex gap-x-1 bg-text hover:bg-subtext0 border border-subtext0 px-4 py-2 rounded-md h-fit shadow-inner shadow-lg transform duration-150"
-      >
-        Dashboard
-      </A>
-      <A
-        href="/"
-        class="text-crust inline-flex gap-x-1 bg-text hover:bg-subtext0 border border-subtext0 pl-3 pr-4 py-2 rounded-md h-fit shadow-inner shadow-lg transform duration-150"
-      >
-        <IconAccount class="text-[16px] my-auto" />
-        My Account
-      </A>
+    <header class="bg-gradient-to-b bg-gradient-from-crust h-64 p-8 flex justify-end">
+      <div class="flex gap-x-4 h-fit">
+        <A
+          href="https://github.com/catto-labs/drive"
+          class="text-crust inline-flex gap-x-1 bg-text hover:bg-subtext0 border border-subtext0 px-4 py-2 rounded-md h-fit my-auto shadow-inner shadow-lg transform duration-150"
+        >
+          Command-line Client
+        </A>
+        <A
+          href="/dashboard"
+          class="text-crust mr-6 inline-flex gap-x-1 bg-text hover:bg-subtext0 border border-subtext0 px-4 py-2 rounded-md h-fit my-auto shadow-inner shadow-lg transform duration-150"
+        >
+          Dashboard
+        </A>
+        <A
+          href="/"
+          class="text-crust inline-flex gap-x-1 bg-text hover:bg-subtext0 border border-subtext0 pl-3 pr-4 py-2 rounded-md h-fit my-auto shadow-inner shadow-lg transform duration-150"
+        >
+          {/* this can be substituted for the user's profile picture instead */}
+          <IconAccount class="text-[16px] my-auto" />
+          My Account
+        </A>
+      </div>
     </header>
   );
 }
