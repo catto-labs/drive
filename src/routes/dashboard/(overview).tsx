fa2707fd275455e0b627e465b62ea427c104d417
jason <development@pnxl.dev>
2023-08-10 20:18:57 +0700

    feat: add new loading screen

--- src/routes/dashboard/(overview).tsx
+++ src/routes/dashboard/(overview).tsx
@@ -1,6 +1,16 @@
-import { type Component, onMount, createSignal, Show, For } from "solid-js";
+import {
+  type Component,
+  onMount,
+  createSignal,
+  createEffect,
+  Show,
+  For,
+} from "solid-js";
 import { Title, useNavigate } from "solid-start";
 import { auth, logOutUser } from "@/stores/auth";
+
+import cattoDriveBox from "@/assets/icon/box.png";
+import cattoDriveCatto from "@/assets/icon/catto.png";
 
 import IconPower from "~icons/mdi/power";
 import IconDotsHorizontalCircleOutline from "~icons/mdi/dots-horizontal-circle-outline";
@@ -8,6 +18,10 @@ import IconStarOutline from "~icons/mdi/star-outline";
 import IconShareVariantOutline from "~icons/mdi/share-variant-outline";
 import IconChevronRight from "~icons/mdi/chevron-right";
 import IconCheck from "~icons/mdi/check";
+import IconFolderUploadOutline from "~icons/mdi/folder-upload-outline";
+import IconFileUploadOutline from "~icons/mdi/file-upload-outline";
+
+import SpinnerRingResize from "~icons/svg-spinners/ring-resize";
 
 import { DropdownMenu } from "@kobalte/core";
 
@@ -89,160 +103,194 @@ const Page: Component = () => {
     }
   };
 
+  createEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <>
       <Title>Dashboard - Drive</Title>
 
-      <div class="backdrop-blur-xl w-screen h-screen relative flex overflow-hidden">
-        <div class="bg-surface0 opacity-50 w-1/5 p-4 shrink-0" />
-
-        <div class="flex flex-col h-full bg-surface0 border-l border-surface2 w-4/5 z-20">
-          <header class="shrink-0 sticky top-0 bg-surface1 border-b border-surface2 w-full h-1/12 flex flex-row justify-between shadow-sm">
-            <h1 class="border-base font-semibold h-full justify-center flex flex-col ml-4">
-              My Files
-            </h1>
-            <div class="flex flex-row gap-x-2 mr-4 items-center">
-              <button
-                type="button"
-                title="Share the selected items"
-                class="hover:text-text text-subtext1 transition hover:bg-surface2 p-1.5 h-fit rounded-lg"
-              >
-                <IconShareVariantOutline class="text-xl" />
-                {/* this should change to a filled star when a file is starred */}
-              </button>
-              <button
-                type="button"
-                title="Toggle favorite"
-                class="hover:text-text text-subtext1 transition hover:bg-surface2 p-1.5 h-fit rounded-lg"
-              >
-                <IconStarOutline class="text-xl" />
-                {/* this should change to a filled star when a file is starred */}
-              </button>
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <button
-                    type="button"
-                    title="Perform tasks with the selected items"
-                    class="hover:text-text text-subtext1 transition hover:bg-surface2 p-1.5 h-fit rounded-lg"
-                  >
-                    <IconDotsHorizontalCircleOutline class="text-xl" />
-                  </button>
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Portal>
-                  <DropdownMenu.Content class="overview-dropdown-content bg-surface0 border border-surface2 p-2 flex flex-col w-68 bg-opacity-50 gap-y-1 backdrop-blur-md rounded-lg text-sm">
-                    <DropdownMenu.Item class="px-4 py-1 hover:bg-lavender rounded-md w-full flex justify-between">
-                      New Folder <span class="text-subtext1">⌘ N</span>
-                    </DropdownMenu.Item>
-                    <DropdownMenu.Item class="px-4 py-1 hover:bg-lavender rounded-md">
-                      Open in New Tab
-                    </DropdownMenu.Item>
-                    <DropdownMenu.Separator class="border border-overlay0 my-1 opacity-50 border-dashed" />
-                    <DropdownMenu.Item class="px-4 py-1 hover:bg-lavender rounded-md w-full flex justify-between">
-                      <div class="inline-flex ">
-                        Quick Look{" "}
-                        <blockquote class="ml-1">My Files</blockquote>
-                      </div>{" "}
-                      <span class="text-subtext1">Space</span>
-                    </DropdownMenu.Item>
-                    <DropdownMenu.Item class="px-4 py-1 hover:bg-lavender rounded-md">
-                      Get Info
-                    </DropdownMenu.Item>
-                    <DropdownMenu.Separator class="border border-overlay0 my-1 opacity-50 border-dashed" />
-                    <DropdownMenu.Item class="px-4 py-1 hover:bg-lavender rounded-md w-full flex justify-between">
-                      Use Groups
-                    </DropdownMenu.Item>
-                    <DropdownMenu.Sub overlap gutter={4} shift={-8}>
-                      <DropdownMenu.SubTrigger class="px-4 py-1 hover:bg-lavender hover:bg-opacity-50 rounded-md flex justify-between w-full overview-dropdown-submenu">
-                        Sort By...
-                        <IconChevronRight class="text-subtext1 text-lg my-auto" />
-                      </DropdownMenu.SubTrigger>
-                      <DropdownMenu.Portal>
-                        <DropdownMenu.SubContent class="overview-dropdown-content bg-surface0 border border-surface2 p-2 bg-opacity-50 gap-y-1 backdrop-blur-md rounded-lg text-sm">
-                          <DropdownMenu.RadioGroup
-                            value={view()}
-                            onChange={setView}
-                            class="flex flex-col w-40 text-sm"
-                          >
-                            <DropdownMenu.RadioItem
-                              class="inline-flex pr-4 pl-5 py-1 hover:bg-lavender rounded-md"
-                              value="name"
-                            >
-                              <DropdownMenu.ItemIndicator class="-ml-4 my-auto">
-                                <IconCheck />
-                              </DropdownMenu.ItemIndicator>
-                              <p class="ml-1">Name</p>
-                            </DropdownMenu.RadioItem>
-                            <DropdownMenu.RadioItem
-                              class="inline-flex pr-4 pl-5 py-1 hover:bg-lavender rounded-md"
-                              value="date-modified"
-                            >
-                              <DropdownMenu.ItemIndicator class="-ml-4 my-auto">
-                                <IconCheck />
-                              </DropdownMenu.ItemIndicator>
-                              <p class="ml-1">Date modified</p>
-                            </DropdownMenu.RadioItem>
-                            <DropdownMenu.RadioItem
-                              class="inline-flex pr-4 pl-5 py-1 hover:bg-lavender rounded-md"
-                              value="kind"
-                            >
-                              <DropdownMenu.ItemIndicator class="-ml-4 my-auto">
-                                <IconCheck />
-                              </DropdownMenu.ItemIndicator>
-                              <p class="ml-1">Kind</p>
-                            </DropdownMenu.RadioItem>
-                            <DropdownMenu.RadioItem
-                              class="inline-flex pr-4 pl-5 py-1 hover:bg-lavender rounded-md"
-                              value="size"
-                            >
-                              <DropdownMenu.ItemIndicator class="-ml-4 my-auto">
-                                <IconCheck />
-                              </DropdownMenu.ItemIndicator>
-                              <p class="ml-1">Size</p>
-                            </DropdownMenu.RadioItem>
-                          </DropdownMenu.RadioGroup>
-                        </DropdownMenu.SubContent>
-                      </DropdownMenu.Portal>
-                    </DropdownMenu.Sub>
-                    <DropdownMenu.Item class="px-4 py-1 hover:bg-lavender rounded-md">
-                      Show View Options
-                    </DropdownMenu.Item>
-                  </DropdownMenu.Content>
-                </DropdownMenu.Portal>
-              </DropdownMenu.Root>
-              <input
-                type="search"
-                placeholder="Search..."
-                name="search"
-                autofocus
-                class="py-1 px-4 rounded-xl w-84 mx-4 bg-surface2 transition border-2 border-overlay0 hover:bg-overlay0 text-text placeholder-text-subtext1"
-              />
-              <button
-                type="button"
-                onClick={async () => {
-                  await logOutUser();
-                  navigate("/");
-                }}
-                title="Logs you out of catto drive"
-                class="hover:text-text text-subtext1 transition hover:bg-surface2 p-1.5 h-fit rounded-lg"
-              >
-                <IconPower class="text-xl" />
-              </button>
+      <Show
+        when={files()}
+        fallback={
+          <div class="bg-surface0 w-screen h-screen relative flex items-center justify-center">
+            <img
+              src={cattoDriveBox}
+              class="absolute mx-auto my-auto w-48 z-10"
+            />
+            <img
+              src={cattoDriveCatto}
+              class="absolute mx-auto my-auto w-41 -mt-10 transition-all"
+              classList={{ "-mt-48": Boolean(files()) }}
+            />
+            <div class="mt-56 text-center">
+              <div class="inline-flex gap-x-2 mt-1">
+                <SpinnerRingResize class="my-auto text-text" />
+                <h1 class="text-lg font-semibold my-auto text-text">
+                  Loading catto drive...
+                </h1>
+              </div>
+              <p class="text-subtext0">
+                Please wait, our cats are finding your files!
+              </p>
             </div>
-          </header>
+          </div>
+        }
+      >
+        <div class="backdrop-blur-xl w-screen h-screen relative flex overflow-hidden">
+          <div class="bg-surface0 opacity-50 w-1/5 p-4 shrink-0" />
 
-          <main class="overflow-auto">
-            <section class="p-4 h-[200px] flex flex-col gap-1 items-center justify-center">
-              <button
-                class="text-2xl font-medium text-text bg-surface0 hover:bg-subtext0 border border-text px-4 py-2 rounded-md duration-150"
+          <div class="flex flex-col h-full bg-surface0 border-l border-surface2 w-4/5 z-20">
+            <header class="shrink-0 sticky top-0 bg-surface1 border-b border-surface2 w-full h-1/12 flex flex-row justify-between shadow-sm">
+              <h1 class="border-base font-semibold h-full justify-center flex flex-col ml-4">
+                My Files
+              </h1>
+              <div class="flex flex-row gap-x-2 mr-4 items-center">
+                <button
+                  type="button"
+                  title="Upload a file"
+                  onClick={() => createFileUpload(fileUploadHandler)}
+                  class="hover:text-text text-subtext1 transition hover:bg-surface2 p-1.5 h-fit rounded-lg"
+                >
+                  <IconFileUploadOutline class="text-xl" />
+                </button>
+                {/* <button
                 type="button"
-                onClick={() => createFileUpload(fileUploadHandler)}
+                title="Upload a folder"
+                class="hover:text-text text-subtext1 transition hover:bg-surface2 p-1.5 h-fit rounded-lg"
               >
-                Upload a file !
-              </button>
-              <p class="text-sm opacity-50">You'll see it's cool</p>
-            </section>
-            <section class="flex flex-wrap gap-4 p-4">
-              <Show when={files()} fallback={<p>Loading files...</p>}>
+                <IconFolderUploadOutline class="text-xl" />
+              </button> */}
+                <button
+                  type="button"
+                  title="Share the selected items"
+                  class="hover:text-text text-subtext1 transition hover:bg-surface2 p-1.5 h-fit ml-4 rounded-lg"
+                >
+                  <IconShareVariantOutline class="text-xl" />
+                </button>
+                <button
+                  type="button"
+                  title="Toggle favorite"
+                  class="hover:text-text text-subtext1 transition hover:bg-surface2 p-1.5 h-fit rounded-lg"
+                >
+                  <IconStarOutline class="text-xl" />
+                  {/* this should change to a filled star when a file is starred */}
+                </button>
+                <DropdownMenu.Root>
+                  <DropdownMenu.Trigger>
+                    <button
+                      type="button"
+                      title="Perform tasks with the selected items"
+                      class="hover:text-text text-subtext1 transition hover:bg-surface2 p-1.5 h-fit rounded-lg"
+                    >
+                      <IconDotsHorizontalCircleOutline class="text-xl" />
+                    </button>
+                  </DropdownMenu.Trigger>
+                  <DropdownMenu.Portal>
+                    <DropdownMenu.Content class="overview-dropdown-content bg-surface0 border border-surface2 p-2 flex flex-col w-68 bg-opacity-50 gap-y-1 backdrop-blur-md rounded-lg text-sm">
+                      <DropdownMenu.Item class="px-4 py-1 hover:bg-lavender rounded-md w-full flex justify-between">
+                        New Folder <span class="text-subtext1">⌘ N</span>
+                      </DropdownMenu.Item>
+                      <DropdownMenu.Item class="px-4 py-1 hover:bg-lavender rounded-md">
+                        Open in New Tab
+                      </DropdownMenu.Item>
+                      <DropdownMenu.Separator class="border border-overlay0 my-1 opacity-50 border-dashed" />
+                      <DropdownMenu.Item class="px-4 py-1 hover:bg-lavender rounded-md w-full flex justify-between">
+                        <div class="inline-flex ">
+                          Quick Look{" "}
+                          <blockquote class="ml-1">My Files</blockquote>
+                        </div>{" "}
+                        <span class="text-subtext1">Space</span>
+                      </DropdownMenu.Item>
+                      <DropdownMenu.Item class="px-4 py-1 hover:bg-lavender rounded-md">
+                        Get Info
+                      </DropdownMenu.Item>
+                      <DropdownMenu.Separator class="border border-overlay0 my-1 opacity-50 border-dashed" />
+                      <DropdownMenu.Item class="px-4 py-1 hover:bg-lavender rounded-md w-full flex justify-between">
+                        Use Groups
+                      </DropdownMenu.Item>
+                      <DropdownMenu.Sub overlap gutter={4} shift={-8}>
+                        <DropdownMenu.SubTrigger class="px-4 py-1 hover:bg-lavender hover:bg-opacity-50 rounded-md flex justify-between w-full overview-dropdown-submenu">
+                          Sort By...
+                          <IconChevronRight class="text-subtext1 text-lg my-auto" />
+                        </DropdownMenu.SubTrigger>
+                        <DropdownMenu.Portal>
+                          <DropdownMenu.SubContent class="overview-dropdown-content bg-surface0 border border-surface2 p-2 bg-opacity-50 gap-y-1 backdrop-blur-md rounded-lg text-sm">
+                            <DropdownMenu.RadioGroup
+                              value={view()}
+                              onChange={setView}
+                              class="flex flex-col w-40 text-sm"
+                            >
+                              <DropdownMenu.RadioItem
+                                class="inline-flex pr-4 pl-5 py-1 hover:bg-lavender rounded-md"
+                                value="name"
+                              >
+                                <DropdownMenu.ItemIndicator class="-ml-4 my-auto">
+                                  <IconCheck />
+                                </DropdownMenu.ItemIndicator>
+                                <p class="ml-1">Name</p>
+                              </DropdownMenu.RadioItem>
+                              <DropdownMenu.RadioItem
+                                class="inline-flex pr-4 pl-5 py-1 hover:bg-lavender rounded-md"
+                                value="date-modified"
+                              >
+                                <DropdownMenu.ItemIndicator class="-ml-4 my-auto">
+                                  <IconCheck />
+                                </DropdownMenu.ItemIndicator>
+                                <p class="ml-1">Date modified</p>
+                              </DropdownMenu.RadioItem>
+                              <DropdownMenu.RadioItem
+                                class="inline-flex pr-4 pl-5 py-1 hover:bg-lavender rounded-md"
+                                value="kind"
+                              >
+                                <DropdownMenu.ItemIndicator class="-ml-4 my-auto">
+                                  <IconCheck />
+                                </DropdownMenu.ItemIndicator>
+                                <p class="ml-1">Kind</p>
+                              </DropdownMenu.RadioItem>
+                              <DropdownMenu.RadioItem
+                                class="inline-flex pr-4 pl-5 py-1 hover:bg-lavender rounded-md"
+                                value="size"
+                              >
+                                <DropdownMenu.ItemIndicator class="-ml-4 my-auto">
+                                  <IconCheck />
+                                </DropdownMenu.ItemIndicator>
+                                <p class="ml-1">Size</p>
+                              </DropdownMenu.RadioItem>
+                            </DropdownMenu.RadioGroup>
+                          </DropdownMenu.SubContent>
+                        </DropdownMenu.Portal>
+                      </DropdownMenu.Sub>
+                      <DropdownMenu.Item class="px-4 py-1 hover:bg-lavender rounded-md">
+                        Show View Options
+                      </DropdownMenu.Item>
+                    </DropdownMenu.Content>
+                  </DropdownMenu.Portal>
+                </DropdownMenu.Root>
+                <input
+                  type="search"
+                  placeholder="Search..."
+                  name="search"
+                  autofocus
+                  class="py-1 px-4 rounded-xl w-84 mx-4 bg-surface2 transition border-2 border-overlay0 hover:bg-overlay0 text-text placeholder-text-subtext1"
+                />
+                <button
+                  type="button"
+                  onClick={async () => {
+                    await logOutUser();
+                    navigate("/");
+                  }}
+                  title="Logs you out of catto drive"
+                  class="hover:text-text text-subtext1 transition hover:bg-surface2 p-1.5 h-fit rounded-lg"
+                >
+                  <IconPower class="text-xl" />
+                </button>
+              </div>
+            </header>
+
+            <main class="overflow-auto">
+              <section class="flex flex-wrap gap-4 p-4">
                 <For each={files()!}>
                   {(file) => (
                     <div class="max-w-60 h-auto rounded-md p-2 flex flex-col gap-1 border border-text hover:bg-subtext0">
@@ -271,11 +319,11 @@ const Page: Component = () => {
                     </div>
                   )}
                 </For>
-              </Show>
-            </section>
-          </main>
+              </section>
+            </main>
+          </div>
         </div>
-      </div>
+      </Show>
     </>
   );
 };
