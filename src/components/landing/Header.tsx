8902bcffc83dbb054f15a33f6032cfa23cbdd76c
jason <development@pnxl.dev>
2023-08-10 19:13:53 +0700

    fix(css): dashboard and myaccount buttons are still dark

--- src/components/landing/Header.tsx
+++ src/components/landing/Header.tsx
@@ -36,14 +36,14 @@ export default function Header() {
         >
           <A
             href="/dashboard"
-            class="text-crust inline-flex gap-x-1 bg-text hover:bg-subtext0 border border-subtext0 px-4 py-2 rounded-md h-fit my-auto shadow-inner shadow-lg transform duration-150"
+            class="text-text inline-flex gap-x-1 bg-surface0 hover:bg-surface1 border border-surface1 px-4 py-2 rounded-md h-fit my-auto shadow-inner shadow-lg transform duration-150"
           >
             Dashboard
           </A>
 
           <A
             href="/"
-            class="text-crust inline-flex gap-x-1 bg-text hover:bg-subtext0 border border-subtext0 pl-3 pr-4 py-2 rounded-md h-fit my-auto shadow-inner shadow-lg transform duration-150"
+            class="text-text inline-flex gap-x-1 bg-surface0 hover:bg-surface1 border border-surface1 pl-3 pr-4 py-2 rounded-md h-fit my-auto shadow-inner shadow-lg transform duration-150"
           >
             {/* this can be substituted for the user's profile picture instead */}
             <IconAccount class="text-[16px] my-auto" />
