a5a85672deefef027fb65adbb268fb5db56fb5ce
trobonox <57040351+trobonox@users.noreply.github.com>
2023-08-11 00:33:03 +0900

    styles; adjust main page colors

--- src/routes/(home).tsx
+++ src/routes/(home).tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div class="bg-surface0 border border-surface2 rounded-lg shadow-xl w-96 h-fit p-4 m-auto absolute left-0 right-0 top-0 bottom-0">
         <div class="flex gap-x-3 py-16 pl-8 justify-start">
           <div class="flex flex-row gap-x-6">
-            <div class="rounded-full p-2 aspect-square m-auto bg-sapphire cursor-pointer hover:bg-[#1a8496] transition-colors duration-200">
+            <div class="rounded-full p-2 aspect-square m-auto bg-lavender cursor-pointer hover:bg-[#5f72d9] transition-colors duration-200">
               <IconPlus class="text-4xl text-mantle" />
             </div>
             <div class="flex flex-col gap-y-1">
@@ -18,11 +18,11 @@ export default function Home() {
               <span class="text-subtext0">Or if you prefer...</span>
               <span class="text-subtext1">
                 select some{" "}
-                <button class="underline-dotted underline hover:underline-solid">
+                <button class="underline-dotted underline hover:underline-solid hover:underline-lavender hover:text-lavender">
                   files
                 </button>{" "}
                 or a{" "}
-                <button class="underline-dotted underline hover:underline-solid">
+                <button class="underline-dotted underline hover:underline-solid hover:underline-lavender hover:text-lavender">
                   folder
                 </button>
                 .
