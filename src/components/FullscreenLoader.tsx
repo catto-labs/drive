b6320a5f7fe3e024fa5ede19ee5638b8c7d41ad8
jason <development@pnxl.dev>
2023-08-11 14:29:40 +0700

    chore: remove unneeded import

--- src/components/FullscreenLoader.tsx
+++ src/components/FullscreenLoader.tsx
@@ -1,4 +1,11 @@
-import { createSignal, type Component, createEffect, on, Show, onMount, onCleanup } from "solid-js";
+import {
+  createSignal,
+  type Component,
+  createEffect,
+  on,
+  Show,
+  onCleanup,
+} from "solid-js";
 
 import cattoDriveBox from "@/assets/icon/box.png";
 import cattoDriveCatto from "@/assets/icon/catto.png";
@@ -9,7 +16,7 @@ import { Motion, Presence } from "@motionone/solid";
 
 const defaultLoaderData = {
   finished: true,
-  message: ""
+  message: "",
 };
 
 const [loaderData, setLoaderData] = createSignal(defaultLoaderData);
@@ -17,35 +24,38 @@ let endLoaderTimeout: ReturnType<typeof setTimeout> | undefined;
 
 export const FullscreenLoaderEntry: Component = () => {
   const [showLoader, setShowLoader] = createSignal(true);
-  createEffect(on(loaderData, (data) => {
-    if (endLoaderTimeout) {
-      clearTimeout(endLoaderTimeout);
-      endLoaderTimeout = setTimeout(() => {
-        setShowLoader(!data.finished);
-        endLoaderTimeout = undefined;
-      }, 1_000);
-    }
-    // If there was no timeout, give the value directly.
-    else setShowLoader(!data.finished);
-  }))
+  createEffect(
+    on(loaderData, (data) => {
+      if (endLoaderTimeout) {
+        clearTimeout(endLoaderTimeout);
+        endLoaderTimeout = setTimeout(() => {
+          setShowLoader(!data.finished);
+          endLoaderTimeout = undefined;
+        }, 1_000);
+      }
+      // If there was no timeout, give the value directly.
+      else setShowLoader(!data.finished);
+    })
+  );
 
   return (
     <Presence exitBeforeEnter>
       <Show when={showLoader()}>
-        <Motion.div class="z-50 bg-surface0 fixed inset-0 flex items-center justify-center"
+        <Motion.div
+          class="z-50 bg-surface0 fixed inset-0 flex items-center justify-center"
           initial={{ opacity: 1 }}
           exit={{ opacity: 0 }}
           transition={{ delay: 0.6, duration: 0.5 }}
         >
-          <img
-            src={cattoDriveBox}
-            class="absolute mx-auto my-auto w-48 z-10"
-          />
+          <img src={cattoDriveBox} class="absolute mx-auto my-auto w-48 z-10" />
 
           <img
             src={cattoDriveCatto}
             class="absolute mx-auto my-auto w-41 transition-all transition-duration-500"
-            classList={{ "-mt-48": loaderData().finished, "-mt-10": !loaderData().finished }}
+            classList={{
+              "-mt-48": loaderData().finished,
+              "-mt-10": !loaderData().finished,
+            }}
           />
 
           <div class="mt-56 text-center">
@@ -58,31 +68,34 @@ export const FullscreenLoaderEntry: Component = () => {
                 {!loaderData().finished ? "Loading..." : "Welcome!"}
               </h1>
             </div>
-            <p class="text-subtext0">
-              {loaderData().message}
-            </p>
+            <p class="text-subtext0">{loaderData().message}</p>
           </div>
         </Motion.div>
       </Show>
     </Presence>
-  )
+  );
 };
 
 const FullscreenLoader: Component<{
-  message: string
+  message: string;
 }> = (props) => {
-  createEffect(on(() =>  props.message, (message) => {
-    setLoaderData({ finished: false, message });
-    onCleanup(() => {
-      // If the message is different, that means that another
-      // loader have taken the lead.
-      if (props.message === loaderData().message) {
-        setLoaderData(defaultLoaderData);
+  createEffect(
+    on(
+      () => props.message,
+      (message) => {
+        setLoaderData({ finished: false, message });
+        onCleanup(() => {
+          // If the message is different, that means that another
+          // loader have taken the lead.
+          if (props.message === loaderData().message) {
+            setLoaderData(defaultLoaderData);
+          }
+        });
       }
-    });
-  }));
-  
+    )
+  );
+
   return <></>;
-}
+};
 
 export default FullscreenLoader;
