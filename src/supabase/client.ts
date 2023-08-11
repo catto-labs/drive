0d7a6fe118ed9fbc5fb2f84ff26c54b4ce2416ba
Mikkel RINGAUD <mikkel@milescode.dev>
2023-08-11 03:46:05 +0200

    feat: get user profile from session

--- src/supabase/client.ts
+++ src/supabase/client.ts
@@ -1,7 +1,19 @@
-import { createClient } from "@supabase/supabase-js";
+import type { UserProfile } from "@/types/api";
+import { Session, createClient } from "@supabase/supabase-js";
 
 // Create a single Supabase client for interacting with your database
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_PROJECT_URL as string,
   import.meta.env.VITE_SUPABASE_ANON_KEY as string
 );
+
+export const getProfileWithSession = async (session: Session) => {
+  const { data: _profile } = await supabase.from("profiles")
+    .select()
+    .eq("user_id", session.user.id)
+    .limit(1)
+    .single();
+      
+  let user_profile = _profile as UserProfile;
+  return user_profile;
+}
