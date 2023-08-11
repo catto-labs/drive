a0c250b7f63cef33f113f1bdb8b343eeaf922ee3
Mikkel RINGAUD <mikkel@milescode.dev>
2023-08-11 03:46:17 +0200

    chore: moved typings

--- src/supabase/server.ts
+++ src/supabase/server.ts
@@ -1,4 +1,4 @@
-import { type UserProfile } from "@/stores/auth";
+import { type UserProfile } from "@/types/api";
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
