1102fd379da13e3d8dd68811c92134dab20012f8
Mikkel RINGAUD <mikkel@milescode.dev>
2023-08-11 03:44:47 +0200

    fix(api/get_file): use redirection and allow public files to be viewed

--- src/routes/api/file/[upload_id].ts
+++ src/routes/api/file/[upload_id].ts
@@ -1,59 +1,73 @@
-import { type APIEvent, json } from "solid-start"
+import { type APIEvent, json, redirect } from "solid-start"
 import { supabase, getUserProfile } from "@/supabase/server";
+import { UploadedFile, UserProfile } from "@/types/api";
 
 export const GET = async ({ request, params }: APIEvent): Promise<Response> => {
   try {
     const { upload_id } = params;
-  
     let token = request.headers.get("authorization");
-    if (!token) return json({
-      success: false,
-      message: "Missing API token."
-    }, { status: 403 });
-  
-    const user_profile = await getUserProfile(token);
-  
+    
     const { data: file_data } = await supabase
       .from("uploads")
       .select()
       .eq("id", upload_id)
       .limit(1)
-      .single();
-  
+      .single<UploadedFile>();
+    
+    // Respond only a 404.
     if (!file_data) return json({
       success: false,
-      message: "File doesn't exist."
-    }, { status: 404 })
-  
-    const { data: workspace_data } = await supabase
-      .from("workspaces")
-      .select()
-      .eq("id", file_data.workspace_id)
-      .limit(1)
-      .single();
-    
-    if (!workspace_data || workspace_data.creator !== user_profile.user_id) return json({
+      message: "Couldn't find any upload with the given ID."
+    }, { status: 404 });
+
+    if (file_data.private && !token) return json({
       success: false,
-      message: "Not allowed to get items from this workspace."
-    }, { status: 403 });
+      message: "You need to give an API token to access this file."
+    }, { status: 401 });
+  
+    let user_profile: UserProfile | undefined;
+    if (token) (user_profile = await getUserProfile(token));
+  
+    // When a file is private, we know
+    // that `workspace_id` and `creator` will not be `null`.
+    // Because only public files can have those!
+    if (file_data.private) {
+      if (!user_profile) return json({
+        success: false,
+        message: "You need to give a valid API token to access this file."
+      }, { status: 401 });
+
+      const { data: workspace_data } = await supabase
+        .from("workspaces")
+        .select()
+        .eq("id", file_data.workspace_id)
+        .limit(1)
+        .single();
+
+      let isCreatorOfFile = file_data.creator === user_profile.user_id;
+      let isCreatorOfWorkspace = workspace_data.creator === user_profile.user_id;
+
+      // Check if we're the owner of the file.
+      if (!isCreatorOfFile && !isCreatorOfWorkspace) return json({
+        success: false,
+        message: "You're not allowed to access this file."
+      }, { status: 403 });
+    }
   
     const file_extension = file_data.name.substring(file_data.name.lastIndexOf(".") + 1);
   
-    // Create a download URL, only available for one minute.
+    // Create a download URL, only available for 15s.
     const { data } = await supabase.storage
       .from("uploads")
-      .createSignedUrl(`${user_profile.user_id}/${file_data.id}.${file_extension}`, 60)
+      .createSignedUrl(`${file_data.creator ?? "anon"}/${file_data.id}.${file_extension}`, 15);
   
-    return json({
-      success: true,
-      data: { url: data?.signedUrl }
-    });
+    return redirect(data!.signedUrl);
   }
   catch (error) {
     console.error(error);
     return json({
       success: false,
-      message: "An error server-side happened"
+      message: "An error server-side happened."
     }, { status: 500 });
   }
 }
