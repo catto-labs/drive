0a302279fb45d07722c0c2cb213e44ec9878c095
jason <development@pnxl.dev>
2023-08-11 13:58:01 +0700

    styles: centre logo and files text

--- src/routes/dashboard/(overview).tsx
+++ src/routes/dashboard/(overview).tsx
@@ -85,16 +85,14 @@ const Page: Component = () => {
       >
         <div class="backdrop-blur-xl w-screen h-screen relative flex overflow-hidden">
           <div class="text-text bg-surface0 bg-opacity-80 min-w-64 w-1/5 shrink-0">
-            <header class="shrink-0 sticky top-0 w-full h-14 flex flex-row justify-between ">
-              <div class="flex mt-1 flex-row gap-[10px] items-center justify-center w-full">
-                <img src={cattoDriveLogo} class="w-10 h-10 -ml-1" />
-                <span class="text-lg">
-                  <span class="font-bold text-2xl -mt-2">Drive </span>
-                  <span class="-mt-2 mr-[10px] font-light text-[15px]">
-                    by catto labs
-                  </span>
+            <header class="shrink-0 sticky top-0 w-full h-14 flex flex-row flex flex-row gap-[10px] items-center justify-center w-full">
+              <img src={cattoDriveLogo} class="w-10 h-10 -ml-1" />
+              <span class="text-lg">
+                <span class="font-bold text-2xl">Drive </span>
+                <span class="mr-[10px] font-light text-[15px]">
+                  by catto labs
                 </span>
-              </div>
+              </span>
             </header>
             <div class="p-4 ">
               <button
@@ -138,7 +136,7 @@ const Page: Component = () => {
           </div>
 
           <div class="flex flex-col h-full bg-surface0 border-l border-surface2 w-4/5 z-20">
-            <header class="shrink-0 sticky top-0 bg-surface1 border-b border-surface2 w-full h-14 flex flex-row justify-between shadow-sm">
+            <header class="shrink-0 sticky top-0 bg-surface1 border-b border-surface2 w-full h-16 flex flex-row justify-between shadow-sm">
               <h1 class="border-base font-semibold h-full justify-center flex flex-col ml-4 text-text">
                 My Files
               </h1>
