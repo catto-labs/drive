482ffb69a53d52ed002064fe7aba23492d14baa1
Mikkel RINGAUD <mikkel@milescode.dev>
2023-08-13 13:41:57 +0200

    fix: add suspense on page load

--- src/root.tsx
+++ src/root.tsx
@@ -17,13 +17,12 @@ import {
   Title,
 } from "solid-start";
 
-import { FullscreenLoaderEntry } from "@/components/FullscreenLoader";
+import FullscreenLoader, { FullscreenLoaderEntry } from "@/components/FullscreenLoader";
 import { getProfileWithSession, supabase } from "@/supabase/client";
 import { setAuth } from "@/stores/auth";
 
 export default function Root() {
   onMount(async () => {
-    window.sb = supabase;
     const { data } = await supabase.auth.getSession();
     if (data.session) {
       const user_profile = await getProfileWithSession(data.session);
@@ -44,7 +43,9 @@ export default function Root() {
       <Body class="min-h-screen h-full bg-[url(https://source.unsplash.com/collection/175083)] bg-no-repeat bg-cover bg-center bg-crust">
         <FullscreenLoaderEntry />
 
-        <Suspense>
+        <Suspense fallback={
+          <FullscreenLoader message="Please wait, our cats are loading your page!" />
+        }>
           <ErrorBoundary>
             <Routes>
               <FileRoutes />
