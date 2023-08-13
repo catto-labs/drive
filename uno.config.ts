1bc420c2101a147e09b8bdbbd84113a28c4ffd2d
Mikkel RINGAUD <mikkel@milescode.dev>
2023-08-13 12:19:02 +0200

    feat: add `createModal` primitive

--- uno.config.ts
+++ uno.config.ts
@@ -1,15 +1,27 @@
-import { defineConfig, presetUno } from "unocss";
+import { type Preset, defineConfig, presetUno } from "unocss";
 import { extendCatppuccin } from "unocss-catppuccin";
 
 import presetRemToPx from "@unocss/preset-rem-to-px";
+import { presetKobalte } from "unocss-preset-primitives";
+
 import transformerVariantGroup from "@unocss/transformer-variant-group";
 
 export default defineConfig({
   presets: [
     presetUno(),
     presetRemToPx(),
+    presetKobalte() as Preset,
     extendCatppuccin({ prefix: "", defaultVariant: "latte" }),
   ],
 
   transformers: [transformerVariantGroup()],
+
+  theme: {
+    animation: {
+      keyframes: {
+        "scale-in": "{from{opacity:0;transform:scale(0.96)}to{opacity:1;transform:scale(1)}}",
+        "scale-out": "{from{opacity:1;transform:scale(&)}to{opacity:0;transform:scale(0.96)}}"
+      }
+    }
+  }
 });
