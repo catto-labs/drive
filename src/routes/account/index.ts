bac8ae3371dbe520d5317db821ea809d77712170
Mikkel RINGAUD <mikkel@milescode.dev>
2023-08-13 20:54:33 +0200

    feat: add endpoint for account update

---
+++ src/routes/account/index.ts
@@ -0,0 +1,33 @@
+import { APIEvent, json } from "solid-start";
+import { getUserProfile, supabase } from "@/supabase/server";
+
+export const POST = async ({ request }: APIEvent) => {
+  let api_token = request.headers.get("authorization");
+  if (!api_token) return json({
+    success: false,
+    message: "You need to provide an API token."
+  }, { status: 401 });
+
+  const user_profile = await getUserProfile(api_token);
+  if (!user_profile) return json({
+    success: false,
+    message: "API key should be wrong."
+  }, { status: 403 });
+
+  const body = await new Response(request.body).json() as {
+    username?: string
+    first_name?: string
+    last_name?: string
+  };
+
+  const { data: updated_user } = await supabase
+    .from('profiles')
+    .update(body)
+    .eq("id", user_profile.user_id)
+    .select()
+
+  return {
+    success: true,
+    data: updated_user
+  }
+}
