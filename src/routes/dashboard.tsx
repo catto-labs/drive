444ea54690e216b2d6b16489a92d9b0192ce7f59
jason <development@pnxl.dev>
2023-08-14 12:37:21 +0700

    styles: made footer fixed

--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -387,7 +387,7 @@ const Layout: Component = () => {
             </header>
             <div class="mt-16"><Outlet /></div>
             
-            <footer class="h-16 w-full flex shrink-0 flex-row gap-x-24 justify-center border-t border-surface1 bg-surface0/30 px-4 shadow-sm md:hidden">
+            <footer class="fixed bottom-0 h-16 w-full flex shrink-0 flex-row gap-x-24 backdrop-blur-md justify-center border-t border-surface1 bg-surface0/30 px-4 shadow-sm md:hidden">
               <A
                 href={`/dashboard/${auth.profile!.root_workspace_id}`}
                 class="flex flex-col items-center gap-1 rounded-md px-2 py-2 transition"
